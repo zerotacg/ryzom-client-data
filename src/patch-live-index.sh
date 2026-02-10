82bf630c8efa51c96e4a09493444e6b68bc1e057
Tobias Peters <tobias.peters@kreativeffekt.at>
2026-02-10 17:39:34 +0100

    adjust scripts to use take file path as input and output parameters where possible

--- src/patch-live-index.sh
+++ src/patch-live-index.sh
@@ -4,14 +4,14 @@ set -e
 
 base_url="https://dl.ryzom.com/patch_live"
 
-version_file=${1:-"version/ryzom_live.version"}
-output_path=${2:-dist}
+version_file="${1:-"dist/ryzom_live.version"}"
+app="$(basename --suffix=".version" -- "$version_file")"
+output="${2:-dist/${app}.idx}"
 
-version_response=$(cat "${version_file}")
+output_dir="$(dirname -- "$output")"
+version_response=$(cat -- "$version_file")
 version=${version_response%% *}
 version_path=$(printf "%05d" "${version}")
-index_url="${base_url}/${version_path}/ryzom_${version_path}.idx"
-output="${output_path}/ryzom.idx"
 
-mkdir --parents "${output_path}"
-curl --fail --location "$index_url" --output "${output}"
+mkdir --parents -- "$output_dir"
+curl --fail --location --show-error "${base_url}/${version_path}/ryzom_${version_path}.idx" --output "${output}"
--- src/patch-live-version.sh
+++ src/patch-live-version.sh
@@ -4,10 +4,10 @@ set -e
 
 base_url="https://dl.ryzom.com/patch_live"
 
-app=${1:-ryzom_live}
-output_path=${2:-dist}
+app="${1:-ryzom_live}"
+output="${2:-dist/${app}.version}"
 
-version_file="${app}.version"
+output_dir="$(dirname -- "$output")"
 
-mkdir --parents "${output_path}"
-curl --fail --location "${base_url}/${version_file}" --output "${output_path}/${version_file}"
+mkdir --parents -- "$output_dir"
+curl --fail --location --show-error "${base_url}/${app}.version" --output "${output}"
